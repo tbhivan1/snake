89fd88181e14df4e0c3d8bce0b87a9ba43067a02
ivan <ivan@DESKTOP-JOPVJST>
2024-03-15 21:33:35 +0100

    commit realizaod correctamente

--- src/PlayerSelectScreen.java
+++ src/PlayerSelectScreen.java
@@ -1,3 +1,4 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,101 +10,106 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 //needs massive aesthetic rewrites
-
 public class PlayerSelectScreen extends JPanel {
-	JButton go;
-	JButton quit;
-	MainWindow mw;
-	
-	JRadioButton opt1;
-	JRadioButton opt2;
-	JRadioButton opt3;
-	
-	public void quitButtonActionListener(){
-		mw.showCard("One");
-	}
-	
-	public void goButtonActionListener(){
-		playerOptions();
-		mw.s4.setUpPlayers();
-		mw.showCard("Three");
-	}
-	
-	public void playerOptions(){
-		int m = 5;
-		if(opt1.isSelected() == true)
-		    m = 1;
-		else if(opt2.isSelected() == true)
-			m = 2;
-		else if(opt3.isSelected() == true)
-			m = 3;
-	    mw.s4.setMaxPlayers(m);
-	}
-	
-	public PlayerSelectScreen(MainWindow mw){
-		this.mw = mw;
-		
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		
-		JLabel mess = new JLabel("Select Players: ");
-		add(mess);
-		
-		JLabel uc = new JLabel("Default: 1Player");
-		add(uc);
-		
-		//set up radio buttons
-		
-		opt1 = new JRadioButton("1 Player(Default)");
-		opt1.setSelected(true);
-		
-		opt2 = new JRadioButton("2 Players");
-		opt3 = new JRadioButton("3 Players");
-		
-		ButtonGroup grp = new ButtonGroup();
-		grp.add(opt1);
-		grp.add(opt2);
-		grp.add(opt3);
-		
-		add(opt1);
-		add(opt2);
-		add(opt3);
 
-		opt1.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				playerOptions();
-			}
-		});
-		opt2.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				playerOptions();
-			}
-		});
-		
-		opt3.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				playerOptions();
-			}
-		});
-		
-		go = new JButton("Customize Board");
-		quit = new JButton("Back");	
-		
-		go.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				goButtonActionListener();
-			}
-		});
-		
-		quit.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				 quitButtonActionListener();
-			}
-		});
-		
-		add(go);
-		add(quit);
-		
-	}
-	
+    JButton go;
+    JButton quit;
+    MainWindow mw;
+
+    JRadioButton opt1;
+    JRadioButton opt2;
+    JRadioButton opt3;
+
+    public void quitButtonActionListener() {
+        mw.showCard("One");
+    }
+
+    public void goButtonActionListener() {
+        playerOptions();
+        mw.s4.setUpPlayers();
+        mw.showCard("Three");
+    }
+
+    public void playerOptions() {
+        int m = 5;
+        if (opt1.isSelected() == true) {
+            m = 1;
+        } else if (opt2.isSelected() == true) {
+            m = 2;
+        } else if (opt3.isSelected() == true) {
+            m = 3;
+        }
+        mw.s4.setMaxPlayers(m);
+    }
+
+    public PlayerSelectScreen(MainWindow mw) {
+        this.mw = mw;
+
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        final String select_Players_ = "Select Players: ";
+
+        JLabel mess = new JLabel(select_Players_);
+        add(mess);
+        final String default_1Player = "Default: 1Player";
+
+        JLabel uc = new JLabel(default_1Player);
+        add(uc);
+        final String _PlayerDefault = "1 Player(Default)";
+
+        //set up radio buttons
+        opt1 = new JRadioButton(_PlayerDefault);
+        opt1.setSelected(true);
+        final String _Players = "2 Players";
+
+        opt2 = new JRadioButton(_Players);
+        final String _Players1 = "3 Players";
+        opt3 = new JRadioButton(_Players1);
+
+        ButtonGroup grp = new ButtonGroup();
+        grp.add(opt1);
+        grp.add(opt2);
+        grp.add(opt3);
+
+        add(opt1);
+        add(opt2);
+        add(opt3);
+
+        opt1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                playerOptions();
+            }
+        });
+        opt2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                playerOptions();
+            }
+        });
+
+        opt3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                playerOptions();
+            }
+        });
+
+        go = new JButton("Customize Board");
+        quit = new JButton("Back");
+
+        go.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                goButtonActionListener();
+            }
+        });
+
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                quitButtonActionListener();
+            }
+        });
+
+        add(go);
+        add(quit);
+
+    }
 
 }
--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -1,3 +1,4 @@
+
 import java.awt.CardLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -9,53 +10,57 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+public class WelcomeScreen extends JPanel {
 
-public class WelcomeScreen extends JPanel{
-	JLabel title;
-	JButton go;
-	JButton quit;
-	
-	MainWindow mw;
-	
-	public void setTitle(String t){
-		title.setText(t);
-	}
-	
-	public void quitButtonActionListener(){
-		if(JOptionPane.showConfirmDialog(this, "Are you sure?") == JOptionPane.OK_OPTION)
-	        System.exit(0);
-	}
-	
-	public void goButtonActionListener(){
-		mw.showCard("Two");
-	}
- 
-	public WelcomeScreen(MainWindow mw){
-		this.mw = mw;
-		
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		title = new JLabel();
-		add(title);
-		
-		//add formatting here
-		
-		go = new JButton("New Game");
-		quit = new JButton("Quit");	
-		
-		go.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				goButtonActionListener();
-			}
-		});
-		
-		quit.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				 quitButtonActionListener();
-			}
-		});
-		
-		add(go);
-		add(quit);
-	}
+    JLabel title;
+    JButton go;
+    JButton quit;
+
+    MainWindow mw;
+
+    public void setTitle(String t) {
+        title.setText(t);
+    }
+    private static final String ARE_YOU_SURE = "Are you sure?";
+
+    public void quitButtonActionListener() {
+        if (JOptionPane.showConfirmDialog(this, ARE_YOU_SURE) == JOptionPane.OK_OPTION) {
+            System.exit(0);
+        }
+    }
+
+    public void goButtonActionListener() {
+        mw.showCard(TWO);
+    }
+    private static final String TWO = "Two";
+
+    public WelcomeScreen(MainWindow mw) {
+        this.mw = mw;
+
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        title = new JLabel();
+        add(title);
+
+        //add formatting here
+        go = new JButton(NEW__GAME);
+        quit = new JButton(QUIT);
+
+        go.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                goButtonActionListener();
+            }
+        });
+
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                quitButtonActionListener();
+            }
+        });
+
+        add(go);
+        add(quit);
+    }
+    private static final String QUIT = "Quit";
+    private static final String NEW__GAME = "New Game";
 
 }
